c65325048c90a2fb57701655156c1301c89fb94d
Yuvraj Singh Bhati <102824251+yuvraj017@users.noreply.github.com>
2022-07-24 21:59:57 +0530

    Add files via upload

---
+++ Java/SimpleDotComTestDrive.java
@@ -0,0 +1,51 @@
+/*---------------------------Main() Class---------------------------*/
+
+public class SimpleDotComTestDrive
+{
+ public static void main(String...arg)
+ {
+  SimpleDotCom dot=new SimpleDotCom();
+
+  int[] loc={2,3,4};
+  
+  dot.setLocationCells(loc);
+  
+  String userGuess="3";
+  String result=dot.checkYourself(userGuess);
+ }
+}
+
+/*---------------------------Problem Class---------------------------*/
+
+class SimpleDotCom
+{
+ int[] locCells;
+ int numOfHits=0;
+ 
+ public void setLocationCells(int[] loc)
+ {
+  locCells=loc;
+ }
+ 
+ public String checkYourself(String stringGuess)
+ {
+  int guess=Integer.parseInt(stringGuess);          //convert String ti Int...
+  String result="miss";
+  
+  for(int cell : locCells)
+  {
+   if(guess==cell)
+   {
+    result="hit";
+    numOfHits++;
+    break;
+   }
+  }
+
+  if(numOfHits==locCells.length)
+  result="kill";
+
+  System.out.println(result);
+  return result;
+ }
+}
